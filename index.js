1f08c39efbfb3de850416342d5e9f05bc256ab93
bizarrebeam <adelyaamanda@gmail.com>
2024-07-12 14:11:04 +0700

    home ejs starting content

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://covers.openlibrary.org/";
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "Booknotes-Capstone",
+  database: "book-shelves",
   password: "adelwebdev2024",
   port: 5432,
 });
@@ -18,158 +18,11 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let search;
-let sort = "review_id";
-
-app.get("/", async (req, res) => { 
-  try {
-    const response = await db.query(
-      `SELECT * FROM books
-      JOIN book_reviews
-      ON books.book_id = book_reviews.book_id
-      ORDER BY ${sort} ASC`
-    )
-
-    const data = response.rows;
-
-    res.render("index.ejs", {
-      search: search,
-      data: data,
-    });
-
-  } catch (err) {
-    console.log("Couldn't execute query for home endpoint: ", err)
-  }
+app.get("/", (req, res) => { 
+  res.render("home.ejs");
 });
 
-app.get("/book", async (req, res) => {
-  const bookTitle = req.query.title;
-  const author = req.query.author;
-  const coverId = req.query.coverId;
 
-  try {
-    const response = await db.query(
-      `SELECT * FROM books
-      JOIN book_reviews
-      ON books.book_id = book_reviews.book_id`
-    )
-
-    const data = response.rows;
-    const bookCheck = data.find((book) => book.title === req.query.title);
-    const review = bookCheck ? bookCheck.review_text : undefined;
-    const highlights = bookCheck ? bookCheck.highlight_text : undefined;
-    const summary = bookCheck ? bookCheck.summary_text : undefined;
-    const book_id = bookCheck ? bookCheck.book_id : undefined;
-
-    res.render("addBook.ejs", {
-      title: bookTitle,
-      author: author,
-      cover: coverId,
-      review: review,
-      highlights: highlights,
-      summary: summary,
-      book_id: book_id,
-    })
-  } catch (err) {
-    console.log("Couldn't execute query for /book: ", err)
-  }
- });
-
-app.post("/sort", async (req, res) => {    
-  sort = req.body.sort;    
-  res.redirect("/");
-})
-
-app.post("/updateReview", async (req, res) => {
-  const summary_text = req.body.summary_text;
-  const review_text = req.body.review_text;
-  const highlight_text = req.body.highlight_text;
-  const rating = req.body.rating;
-
-  const book_id = req.body.bookId;
-
-  const currentDate = new Date().toISOString();
-
-  try {
-    const response = await db.query(
-      `UPDATE book_reviews
-      SET summary_text = $1, review_text = $2, highlight_text = $3, rating = $4, review_date = $5
-      WHERE book_id = $6
-      RETURNING *`, [summary_text, review_text, highlight_text, rating, currentDate, book_id]
-    )
-
-    const data = response.rows;
-    console.log("Succesfully update", `${summary_text}, ${review_text}, ${highlight_text}, ${rating}, ${book_id}, ${currentDate}`);
-    res.redirect("/");
-
-  } catch (err) {
-    console.log("Couldn't execute query for /updateReview: ", err)
-  }
-})
-
-app.post("/addBook", async (req, res) => {
-  const bookTitle = req.body.title;
-  const coverId = req.body.cover_id;
-  const author = req.body.author;
-
-  const summary_text = req.body.summary_text;
-  const review_text = req.body.review_text;
-  const highlight_text = req.body.highlight_text;
-  const rating = req.body.rating;
-
-  const currentDate = new Date().toISOString();
-
-  try {
-    await db.query('BEGIN');
-  
-    const bookResponse = await db.query(
-      `INSERT INTO books (title, author, cover_id)
-      VALUES ($1, $2, $3)
-      RETURNING *`, [bookTitle, author, coverId]
-    );
-  
-    const bookData = bookResponse.rows;
-    const book_id = bookData[0].book_id;
-  
-    const reviewResponse = await db.query(
-      `INSERT INTO books_reviews (book_id, summary_text, review_text, highlight_text, rating, review_date)
-      VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING *`, [book_id, summary_text, review_text, highlight_text, rating, currentDate]
-    );
-  
-    await db.query('COMMIT');
-  
-    console.log("Successfully added", `${bookTitle}, ${author}, ${coverId}, ${summary_text}, ${review_text}, ${highlight_text}, ${rating}, ${currentDate}`);
-    res.redirect("/");
-  
-  } catch (err) {
-    await db.query('ROLLBACK');
-    console.log("Couldn't execute query for /addBook: ", err);
-  }
-
-})
-
-app.delete("/delete/:id", async (req, res) => {
-  const bookId = req.params.id;
-
-  try {
-    await db.query('BEGIN');
-    await db.query(
-      `DELETE FROM book_reviews
-      WHERE book_id = $1`, [bookId]
-    )
-    await db.query(
-      `DELETE FROM books
-      WHERE book_id = $1`, [bookId]
-    )
-    await db.query('COMMIT');
-    res.redirect("/");
-
-  } catch (err) {
-    await db.query('ROLLBACK');
-    console.log("Couldn't execute query for /delete: ", err)
-  }
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
--- queries.sql
+++ queries.sql
@@ -2,15 +2,30 @@ CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
     title VARCHAR(255) UNIQUE NOT NULL,
     author VARCHAR(255),
-    cover_id VARCHAR(13)    
+    isbn VARCHAR(13) UNIQUE NOT NULL,  -- ISBN used for fetching the cover
+    language VARCHAR(50),
+    finished_month_year VARCHAR(20)  -- Store month and year as text
 );
 
 CREATE TABLE book_reviews (
     review_id SERIAL PRIMARY KEY,
     book_id INTEGER REFERENCES books(book_id),
-    rating INTEGER CHECK (rating >= 1 AND rating <= 5),
     summary_text TEXT,
-    review_text TEXT,
-    highlight_text TEXT,
-    review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    highlight_text TEXT[], 
+);
+
+-- populate first entry
+INSERT INTO books (title, author, isbn, language, finished_month_year)
+VALUES ('A Tale for the Time Being', 'Ruth Ozeki', '9780143124870', 'English', 'June 2024');
+
+INSERT INTO book_reviews (book_id, summary_text, highlight_text)
+VALUES 
+(1, 
+ 'A story that alternates between a Japanese schoolgirl, Nao, and a Canadian author, Ruth. Ruth finds Nao\'s diary washed up on the shore where she lives and presumes the 2011 Tōhoku tsunami is what brought the diary to her. The story unfolds with Ruth becoming absorbed, page by page, in finding out what ultimately happened to Nao, as Nao pours out her difficult life through the diary. What initially intrigued me was whether the tsunami really connected Nao\'s life to Ruth through the diary. However, I found myself not in a hurry to uncover the truth. Instead, I paced slowly with Nao\'s tale. It\'s heartbreaking as Nao shares her life spanning three generations: her suicidal father, her Buddhist nun great-grandmother, and her deceased kamikaze grand-uncle. She recounts the bullying and harassment she endures every day in a teen\'s language, as if it were normal reality to her. The opening of the book was, "Hi! My name is Nao, and I am a time being." From that point on, I positioned myself as Ruth, slowly uncovering the pages without haste, and allowing myself to be a time being—whether present or past—connected with Nao\'s time.', 
+ ARRAY[
+    'Or maybe none of these things will happen except in my mind and yours, because, like I told you, together we’re making magic, at least for the time being.', 
+    'Do not think that time simply flies away. Do not understand “flying” as the only function of time. If time simply flew away, a separation would exist between you and time. So if you understand time as only passing, then you do not understand the time being. To grasp this truly, every being that exists in the entire world is linked together as moments in time, and at the same time they exist as individual moments of time. Because all moments are the time being, they are your time being.',
+    'What if I travel so far away in my dream that I can’t get back in time to wake up?',
+    'As I have not much time left in life, I am determined not to be a coward. I will live as earnestly as I can and feel my feelings deeply. I will rigorously reflect upon my thoughts and emotions, and try to improve myself as much as I can.'
+ ]
 );
