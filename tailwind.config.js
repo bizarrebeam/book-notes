7aa35714b88e602c21de9eadb0ffa1fd005a3b28
bizarrebeam <adelyaamanda@gmail.com>
2025-08-20 14:11:57 +0700

    custom color variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
       fontFamily: {
         'poppins': ["Poppins", "sans-serif"], 
       },
+      colors: {
+        'brown-1': '#463F3A',
+        'brown-2': '#9F9F92', 
+        'brown-3': '#F4F1EA',
+        'peach': '#E0AFA0',
+        'white': '#FFFFFF'
+      },
       boxShadow: {
         'text': '2px 2px 4px rgba(0, 0, 0, 0.5)', // Custom shadow for text
       },
